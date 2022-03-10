fbdba77c0d8b95f565363c8768882eef9ea2382a
Jacob <jas111213@gmail.com>
2022-03-10 10:20:36 -0500

    query, flow, change, content, extra pages

---
+++ mouseover.js
@@ -0,0 +1,8 @@
+// JavaScript function changes and id content.
+// Parameter passing is the key to understanding this function.
+// this first JavaScript function
+
+function change(id,img)
+{
+document.getElementById(id).src=img;
+}
